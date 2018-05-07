9924803222f86a54b98de70cf60e2982bf095e8f
Shawn Hoover <shawn.hoover@gmail.com>
2018-05-06 20:15:50 -0400

    Pass bootstrap destination to installer.

--- init/bootstrap.sh
+++ init/bootstrap.sh
@@ -1,6 +1,9 @@
 dst=${1:-$HOME}
 
+echo Bootstrap installing dotfiles to $dst
+
 set -e
+set -x
 
 cd /tmp
 wget -O dotfiles.tar.gz https://bitbucket.org/shoover/dotfiles/get/default.tar.gz
@@ -15,4 +18,4 @@ rm -rf /tmp/dotfiles
 mkdir -p $dst
 cd $dst
 hg clone ssh://hg@bitbucket.org/shoover/dotfiles
-bash -c dotfiles/init/install.sh
+bash -c dotfiles/init/install.sh $dst
