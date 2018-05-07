e8874362666317abf3cb3da02a30536e767b8a68
Shawn Hoover <shawn.hoover@gmail.com>
2018-05-06 20:07:59 -0400

    u+x

--- init/bootstrap.sh
+++ init/bootstrap.sh
@@ -1,11 +1,16 @@
 dst=${1:-$HOME}
 
+set -e
+
 cd /tmp
 wget -O dotfiles.tar.gz https://bitbucket.org/shoover/dotfiles/get/default.tar.gz
-tar xzf dotfiles.tar.gz
+mkdir dotfiles
+tar xzf dotfiles.tar.gz --strip 1 -C dotfiles
 
 bash -c dotfiles/init/00-platform.sh
 bash -c dotfiles/init/01-hg.sh
+
+rm -rf /tmp/dotfiles
 
 mkdir -p $dst
 cd $dst
