79b698bd388b34216739feb666e5b540de14f44b
Shawn Hoover <shawn.hoover@gmail.com>
2018-06-08 12:06:23 -0400

    Option for bootstrapping dotfiles only

--- init/bootstrap.sh
+++ init/bootstrap.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
+# ./bootstrap.sh [DEST|$HOME] [dotfiles|all]
+# ./bootstrap.sh                  # default install files to HOME, run installers, clone repo
+# ./bootstrap.sh $HOME dotfiles   # dotfiles only, no installers, no clone (no root)
+
 dst=$(realpath ${1:-$HOME})
+selection=${2:all}
 
 echo Bootstrap installing dotfiles to $dst
 
@@ -10,6 +17,21 @@ cd /tmp
 wget -O dotfiles.tar.gz https://bitbucket.org/shoover/dotfiles/get/default.tar.gz
 mkdir dotfiles
 tar xzf dotfiles.tar.gz --strip 1 -C dotfiles
+
+# Dotfiles only, for non-root, quick setup
+if [ "$selection" == "dotfiles" ]
+then
+    . dotfiles/init/install.sh $dst $selection
+    exit
+fi
+
+if [ "$selection" != "all" ]
+then
+    echo Invalid selection: $selection
+    exit 1
+fi
+
+# Continue with hg checkout and package installation
 
 bash -c dotfiles/init/00-platform.sh
 bash -c dotfiles/init/01-hg.sh
