cd2ab11c39485510c42e74f090f5aa7b4caaca1d
Shawn Hoover <shawn.hoover@gmail.com>
2019-02-21 15:53:16 -0500

    Switch the emacs repo URL to https, too

--- init/12-emacs.sh
+++ init/12-emacs.sh
@@ -1,7 +1,7 @@
 pushd $DEST
 
 if ! [ -d "emacs" ]; then
-    hg clone ssh://hg@bitbucket.org/shoover/emacs.d emacs
+    hg clone https://bitbucket.org/shoover/emacs.d emacs
 fi
 
 emacs --load emacs/bootstrap.el --batch --funcall my-bootstrap-packages
