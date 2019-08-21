214ec13c1c244ed5940078044553253349e43f33
Shawn Hoover <shawn.hoover@gmail.com>
2019-08-20 22:44:28 -0400

    Upgrade python packages.

--- init/11-hgext.sh
+++ init/11-hgext.sh
@@ -1,8 +1,8 @@
 # hg extension packages
-pip install --user hg-git
-pip install --user hg-evolve
-pip install --user keyring
-pip install --user mercurial_keyring
+pip install --user -U hg-git
+pip install --user -U hg-evolve
+pip install --user -U keyring
+pip install --user -U mercurial_keyring
 
 # hg extensions from source
 mkdir -p $DEST/dev/hgext
