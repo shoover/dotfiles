ac8705c67bf9dd94e263bc6c5f8e2d3ebac511f3
Shawn Hoover <shawn.hoover@gmail.com>
2021-10-02 17:10:55 -0400

    zsh

---
+++ init/22-zsh.sh
@@ -0,0 +1,9 @@
+sudo apt install -y zsh
+chsh -s $(which zsh)
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+pip3 install Markdown powerline-status
+
+# https://blog.joaograssi.com/windows-subsystem-for-linux-with-oh-my-zsh-conemu/
+curl https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.ansi-dark --output ~/.dircolors
