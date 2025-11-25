44e9430f94380470b6382e12ac914b8f31271bd9
Shawn Hoover <shawn@shawnhoover.dev>
2025-11-24 22:44:21 -0500

    Prompt for git email
    - This allows machine-specific (work vs. personal) email addresses while
    - generally sharing gitconfig
    - Store email in a separate included gitconfig, generated from user input

---
+++ macos/tools/12-git.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Create .gitconfig.local to allow setting a machine-specific email interactively.
+
+set -e
+
+config_path="$DEST/.gitconfig.local"
+default_email=shawn@shawnhoover.dev
+
+if [ -f "$config_path" ]; then
+  echo "$config_path already exists. Skipping."
+  exit 0
+fi
+
+if [ -t 0 ]; then
+  read -r -p "Preferred git email [${default_email}]: " email
+else
+  echo "No TTY available to read preferred git email. Skipping."
+  exit 0
+fi
+
+if [ -z "$email" ]; then
+    email=$default_email
+fi
+
+echo "Configuring email ${email}"
+
+cat > "$config_path" <<CONF
+[user]
+	name = Shawn Hoover
+	email = ${email}
+CONF
